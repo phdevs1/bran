a98b9bec53c11a57923760d623ae560e828b8422
pio <pio.huisa@gmail.com>
2015-05-18 11:04:24 -0500

    sbueindo

--- pru.cpp
+++ pru.cpp
@@ -4,21 +4,9 @@
 
 int main(int argc, char const *argv[])
 {
-	cout<<"hello2";
-		cout<<"hello3";
-		cout<<"hello4";
-		cout<<"hello5";
+	cout<<"hello1"<<endl;
+	
 
-		cout<<"hello6";
-
-				cout<<"tmr";
-								cout<<"tmr3";
-<<<<<<< HEAD
-			cout<<"tmr5";
-cout<<"hello2";
-=======
-		cout<<"tmr1231";
->>>>>>> master
 	return 0;
 }
 
